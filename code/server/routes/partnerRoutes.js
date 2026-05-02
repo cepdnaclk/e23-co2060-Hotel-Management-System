5f94636294e33c9a0485cd896706fe685a981ed6
Isindu Anusra <e23015@eng.pdn.ac.lk>
2026-05-02 07:59:04 +0530

    Remove old code folder from final branch

--- code/server/routes/partnerRoutes.js
+++
@@ -1,9 +0,0 @@
-const express = require("express");
-const router = express.Router();
-
-const { registerPartner, loginPartner } = require("../controllers/partnerController");
-
-router.post("/register", registerPartner);
-router.post("/login", loginPartner);
-
-module.exports = router;
